907b7392d3c9c3429be1ab4bb30e126cfeef4269
Agustina Nahir Ruidiaz <agusruidiaz@hotmail.com>
2020-10-26 20:19:16 -0300

    corrigiendo funcionalidades

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const crearFila = (dato) =>{
     
     const trow = document.createElement("tr");
     trow.className = 'tr-style'; // TODO: estilar tr-style !!!!!!!!!!!!!!!!!!!!!!!!!!
-    //trow.id = `Producto-${idProd}`;
+    trow.id = `user-${dato.id}`;
     const tCheckbox = document.createElement('td');
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
@@ -40,6 +40,16 @@ const crearFila = (dato) =>{
     iDelete.classList.add("material-icons");
     iDelete.title="Delete";
 
+    buttonDelete.addEventListener("click", () => {
+        //sacamos del html:
+        eliminarFila(dato.id);
+        //sacamos de la api:
+        axios.delete(`https://5f7c70d600bd74001690ac5e.mockapi.io/users/${dato.id}`)
+            .then(res => console.log(dato))
+            .catch(err => console.log(err)); 
+    });
+
+
     buttonEdit.appendChild(iEdit);
     tAction.appendChild(buttonEdit);
     buttonDelete.appendChild(iDelete);
@@ -57,8 +67,6 @@ const crearFila = (dato) =>{
 
 }
 
-
-
 const cargarDatos = async () =>{
     try{
         const res = await axios.get('https://5f7c70d600bd74001690ac5e.mockapi.io/users')
@@ -74,7 +82,48 @@ const cargarDatos = async () =>{
     }
 }
 
+const eliminarFila = (idUser) => {
+    const userAeliminar = document.querySelector(`#user-${idUser}`);
+    userAeliminar.remove();
+}
+
+const eliminarTodasLasFilas = (fila) =>{
+    let list = document.querySelectorAll(fila);
+    for(let i of list){
+        i.remove();
+    }
+}
+
+
+const filtrarDatos = async (searchParam) =>{
+    try{
+        const res = await axios.get(`https://5f7c70d600bd74001690ac5e.mockapi.io/users/?search=${searchParam}`);
+        eliminarTodasLasFilas('.tr-style');
+        datos = res.data;
+        console.log(datos);
+        datos.map( dato =>{
+            crearFila(dato);
+        })
+        
+    }catch(err){
+        console.log('ERROR:',err);
+    }
+}
+
+const filtrarUsuarios = () =>{
+    const searchParam = document.querySelector("#input-filter-user").value.toLowerCase();
+    console.log(searchParam);
+    if(searchParam.length < 3) return; 
+    //if(searchParam == "") cargarDatos();
+    filtrarDatos(searchParam);
+}
+
+const eliminarUser = () =>{
+
+}
+
 const onLoad = () => {
     cargarDatos();
+    document.querySelector("#btn-filter-user").addEventListener("click" ,filtrarUsuarios);
     //para poner los add event listeners
 }
